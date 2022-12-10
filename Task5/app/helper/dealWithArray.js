c0c5f5370d118e446c4f4638d5a8ef0e17159552
AbdulrahmanAshraf98 <abdulrahmanashraf9898@gmail.com>
2022-12-10 20:48:44 +0200

    task 5

--- Task5/app/helper/dealWithArray.js
+++ Task5/app/helper/dealWithArray.js
@@ -2,64 +2,61 @@ const checkValidArray = (array) => {
 	if (array === null || array.length === 0) return false;
 	return true;
 };
-const getIndexOfElement = (array, key, value) => {
-	const validArray = checkValidArray(array);
-	if (!validArray || !key || !value) return null;
-	return array.findIndex((item) => item[key] === value);
-};
-const getElementBy = (array, key, value) => {
-	const validArray = checkValidArray(array);
-	if (!validArray || !key || !value) return null;
-	const element = array.find((item) => item[key] === value);
-	return element ? element : null;
-};
-const deleteElementFromArray = (array, key, value) => {
-	const validArray = checkValidArray(array);
-	if (!validArray || !key || !value) return [];
-	const newArray = array.filter((item) => item[key] !== value);
-	return newArray;
-};
-const sortArrayBy = (
-	array,
-	type = "normalArray",
-	key = "",
-	keyType = "string",
-	sortOrder = "asc",
-) => {
-	if (!key) return array.sort();
-	if (type === "normalArray")
-		return sortOrder === "asc"
-			? array.sort((a, b) => a - b)
-			: array.sort((a, b) => b - a);
-	if (type === "arrayOfObject")
-		if (keyType === "string") {
+class DealWithArray {
+	static getIndexOfElement = (array, key, value) => {
+		const validArray = checkValidArray(array);
+		if (!validArray || !key || !value) return null;
+		return array.findIndex((item) => item[key] === value);
+	};
+	static getElementBy = (array, key, value) => {
+		const validArray = checkValidArray(array);
+		if (!validArray || !key || !value) return null;
+		const element = array.find((item) => item[key] === value);
+		return element ? element : null;
+	};
+	static deleteElementFromArray = (array, key, value) => {
+		const validArray = checkValidArray(array);
+		if (!validArray || !key || !value) return [];
+		const newArray = array.filter((item) => item[key] !== value);
+		return newArray;
+	};
+	static sortArrayBy = (
+		array,
+		type = "normalArray",
+		key = "",
+		keyType = "string",
+		sortOrder = "asc",
+	) => {
+		if (!key) return array.sort();
+		if (type === "normalArray")
 			return sortOrder === "asc"
-				? array.sort((a, b) => a[key].localeCompare(b[key]))
-				: array.sort((a, b) => a[key].localeCompare(b[key]));
-		}
-	return sortOrder === "asc"
-		? array.sort((a, b) => a[key] - b[key])
-		: array.sort((a, b) => b[key] - a[key]);
-};
-const searchForStringInArray = (
-	array,
-	value,
-	typeOFArray = "normalArray",
-	key = "",
-) => {
-	if (typeOFArray === "arrayOfObject")
+				? array.sort((a, b) => a - b)
+				: array.sort((a, b) => b - a);
+		if (type === "arrayOfObject")
+			if (keyType === "string") {
+				return sortOrder === "asc"
+					? array.sort((a, b) => a[key].localeCompare(b[key]))
+					: array.sort((a, b) => a[key].localeCompare(b[key]));
+			}
+		return sortOrder === "asc"
+			? array.sort((a, b) => a[key] - b[key])
+			: array.sort((a, b) => b[key] - a[key]);
+	};
+	static searchForStringInArray = (
+		array,
+		value,
+		typeOFArray = "normalArray",
+		key = "",
+	) => {
+		if (typeOFArray === "arrayOfObject")
+			return array.filter((element) =>
+				element[key].trim().toLowerCase().includes(value.trim().toLowerCase()),
+			);
 		return array.filter((element) =>
-			element[key].trim().toLowerCase().includes(value.trim().toLowerCase()),
+			element.trim().toLowerCase().includes(value.trim().toLowerCase()),
 		);
-	return array.filter((element) =>
-		element.trim().toLowerCase().includes(value.trim().toLowerCase()),
-	);
-};
+	};
+}
 
-module.exports = {
-	getIndexOfElement,
-	getElementBy,
-	deleteElementFromArray,
-	sortArrayBy,
-	searchForStringInArray,
-};
+
+module.exports = DealWithArray;
